f795c945b283c64b83667a2bb750ecd956592431
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-01-29 16:45:07 +0100

    Create Sliding.cs

---
+++ Scripts/Movement/Sliding.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sliding : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform orientation;
+    [SerializeField] private Transform playerObj;
+    private Rigidbody rb;
+    private PlayerMovementAdvanced pm;
+
+    [Header("Sliding")]
+    [SerializeField] private float maxSlideTime;
+    [SerializeField] private float slideForce;
+    private float slideTimer;
+
+    [SerializeField] private float slideYScale;
+    private float startYScale;
+
+    [Header("Input")]
+    [SerializeField] private KeyCode slideKey = KeyCode.LeftControl;
+    private float horizontalInput;
+    private float verticalInput;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pm = GetComponent<PlayerMovementAdvanced>();
+
+        startYScale = playerObj.localScale.y;
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(slideKey) && (horizontalInput != 0 || verticalInput != 0))
+            StartSlide();
+
+        if (Input.GetKeyUp(slideKey) && pm.sliding)
+            StopSlide();
+    }
+
+    private void FixedUpdate()
+    {
+        if (pm.sliding)
+            SlidingMovement();
+    }
+
+    /// <summary>
+    /// Starts the slide and scales down the player.
+    /// </summary>
+    private void StartSlide()
+    {
+        pm.sliding = true;
+
+        playerObj.localScale = new Vector3(playerObj.localScale.x, slideYScale, playerObj.localScale.z);
+        rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+
+        slideTimer = maxSlideTime;
+    }
+
+    /// <summary>
+    /// Handles the movement while sliding and builds speed on slopes.
+    /// </summary>
+    private void SlidingMovement()
+    {
+        Vector3 inputDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        // sliding normal
+        if(!pm.OnSlope() || rb.velocity.y > -0.1f)
+        {
+            rb.AddForce(inputDirection.normalized * slideForce, ForceMode.Force);
+
+            slideTimer -= Time.deltaTime;
+        }
+
+        // sliding down a slope
+        else
+        {
+            rb.AddForce(pm.GetSlopeMoveDirection(inputDirection) * slideForce, ForceMode.Force);
+        }
+
+        if (slideTimer <= 0)
+            StopSlide();
+    }
+
+    /// <summary>
+    /// Stops the slide and scales the player back.
+    /// </summary>
+    private void StopSlide()
+    {
+        pm.sliding = false;
+
+        playerObj.localScale = new Vector3(playerObj.localScale.x, startYScale, playerObj.localScale.z);
+    }
+}
