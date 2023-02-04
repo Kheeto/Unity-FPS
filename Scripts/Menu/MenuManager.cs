d8e9170cb58a30175f0fcb8132d9f7824d106589
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-02-04 22:39:01 +0100

    Update MenuManager.cs

--- Scripts/Menu/MenuManager.cs
+++ Scripts/Menu/MenuManager.cs
@@ -15,6 +15,7 @@ public class MenuManager : MonoBehaviour
 
     private bool menuOn = false;
     private bool gameEnded = false;
+    private bool dead = false;
 
     private void Update()
     {
@@ -24,6 +25,7 @@ public class MenuManager : MonoBehaviour
     private void TogglePauseMenu()
     {
         if (gameEnded) return;
+        if (dead) return;
 
         menuOn = !menuOn;
 
@@ -31,28 +33,55 @@ public class MenuManager : MonoBehaviour
         {
             pauseMenu.SetActive(true);
             Time.timeScale = 0f;
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
         else
         {
             pauseMenu.SetActive(false);
             Time.timeScale = 1f;
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
+    }
+
+    public void ShowDeathMenu()
+    {
+        if (gameEnded) return;
+        dead = true;
+
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     public void ShowEndMenu()
     {
+        gameEnded = true;
+
         pauseMenu.SetActive(false);
         endMenu.SetActive(true);
         Time.timeScale = 0f;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     public void Next()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Restart()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
