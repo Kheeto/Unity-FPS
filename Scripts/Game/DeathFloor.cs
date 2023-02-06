41a1f4c0d603d39f8ffb1d8656045c237ef80eb9
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-02-06 18:38:01 +0100

    Update DeathFloor.cs

--- Scripts/Game/DeathFloor.cs
+++ Scripts/Game/DeathFloor.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class DeathFloor : MonoBehaviour
 {
+    [Header("Rising Floor")]
+    [SerializeField] private float speed;
+
+    [Header("References")]
     [SerializeField] private Transform player;
     [SerializeField] private MenuManager menuManager;
+
+    private void Update() {
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y + speed * Time.deltaTime,
+            transform.position.z);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
