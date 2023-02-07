063e8c1868cac8ca93a2356ce17dfffdf94273e7
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-02-07 19:21:53 +0100

    Update Button.cs

--- Scripts/Game/Button.cs
+++ Scripts/Game/Button.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-    [Header("Button settings")]
     [SerializeField] private List<Door> doors = new List<Door>();
+
+    [Header("Button animation")]
+    [SerializeField] private Vector3 defaultPosition;
+    [SerializeField] private Vector3 pressedPosition;
+    [SerializeField] private float clickSpeed = 1f;
+
+    private bool pressed = false;
+
+    private void Start()
+    {
+        pressed = false;
+        transform.localPosition = defaultPosition;
+    }
 
     private void OnMouseDown()
     {
+        pressed = true;
+
         foreach (Door door in doors)
         {
             door.ToggleDoor(true);
         }
+    }
+
+    private void Update()
+    {
+        if (pressed)
+            transform.localPosition = Vector3.Lerp(transform.localPosition, pressedPosition,
+                clickSpeed * Time.deltaTime);
     }
 }
