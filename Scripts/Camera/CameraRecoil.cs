e7f0f0c67baea486e4483035b24ce403e1c6894a
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-02-09 22:10:18 +0100

    Update CameraRecoil.cs

--- Scripts/Camera/CameraRecoil.cs
+++ Scripts/Camera/CameraRecoil.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class CameraRecoil : MonoBehaviour
 {
     [Header("Recoil Settings")]
-    [SerializeField] private Vector3 recoil;
-    [SerializeField] private float roughness;
-    [SerializeField] private float returnSpeed;
+    [SerializeField] private Vector3 recoil = new Vector3(-2f, 1f, 2f);
+    [SerializeField] private Vector3 returnSpeed = new Vector3(1f, 3f, 5f);
+    [SerializeField] private float roughness = 13f;
 
     private Vector3 currentRot;
     private Vector3 targetRot;
 
     private void Update()
     {
-        targetRot = Vector3.Lerp(targetRot, Vector3.zero, returnSpeed * Time.deltaTime);
+        targetRot = new Vector3(
+            Mathf.Lerp(targetRot.x, 0f, returnSpeed.x * Time.deltaTime),
+            Mathf.Lerp(targetRot.y, 0f, returnSpeed.y * Time.deltaTime),
+            Mathf.Lerp(targetRot.z, 0f, returnSpeed.z * Time.deltaTime)
+        );
         currentRot = Vector3.Slerp(currentRot, targetRot, roughness * Time.deltaTime);
 
         transform.localRotation = Quaternion.Euler(currentRot);
