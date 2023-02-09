6a80a46d9a94f987db4b43d47a5a32123133430c
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-02-09 19:21:21 +0100

    Create CameraRecoil.cs

---
+++ Scripts/Camera/CameraRecoil.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRecoil : MonoBehaviour
+{
+    [Header("Recoil Settings")]
+    [SerializeField] private Vector3 recoil;
+    [SerializeField] private float roughness;
+    [SerializeField] private float returnSpeed;
+
+    private Vector3 currentRot;
+    private Vector3 targetRot;
+
+    private void Update()
+    {
+        targetRot = Vector3.Lerp(targetRot, Vector3.zero, returnSpeed * Time.deltaTime);
+        currentRot = Vector3.Slerp(currentRot, targetRot, roughness * Time.fixedDeltaTime);
+
+        transform.localRotation = Quaternion.Euler(currentRot);
+    }
+
+    public void ApplyRecoil()
+    {
+        targetRot += new Vector3(recoil.x, Random.Range(-recoil.y, recoil.y), Random.Range(-recoil.z, recoil.z));
+    }
+}
